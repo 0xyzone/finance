94e4f0a1ccb114ddab8c0c7c1273c362dc89992a
0xyzone <58982560+0xyzone@users.noreply.github.com>
2022-06-18 02:33:02 +0545

    just an update

--- public/login/index.php
+++ public/login/index.php
@@ -9,7 +9,7 @@ if (isset($_SESSION['vcaf_user'])) {
     <section class="signup">
         <div class="text-2xl font-bold text-white">Log In</div>
         <form action="login.php" method="POST" id="signup">
-            <input type="text" name="uname" id="uname" placeholder="Username/Email Address/Phone" autocomplete="off" required>
+            <input type="text" name="uname" id="uname" placeholder="Username/Email Address/Phone" autofocus="on" required>
             <input type="password" name="pw" id="pw" placeholder="Password" required>
             <button for="signup" type="submit" class="w-max btn-primary mx-auto mt-2">Log In</button>
         </form>
--- public/signup/index.php
+++ public/signup/index.php
@@ -4,7 +4,7 @@ include "../includes/header.php"
 <section class="signup">
     <div class="text-2xl font-bold text-white">Sign Up</div>
     <form action="signup.php" method="POST" id="signup">
-    <input type="text" name="uname" id="uname" placeholder="Username" autocomplete="off" required>
+    <input type="text" name="uname" id="uname" placeholder="Username" autofocus="on" required>
     <input type="email" name="email" id="email" placeholder="Email Address" required>
     <input type="tel" name="tel" id="tel" placeholder="Contact No." required>
     <input type="password" name="pw" id="pw" placeholder="Password" required>
