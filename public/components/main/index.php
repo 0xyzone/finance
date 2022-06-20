3df00d1b54af6f24ff70e5f9c6064f954e48327b
0xyzone <58982560+0xyzone@users.noreply.github.com>
2022-06-21 02:46:34 +0545

    added main.php

---
+++ public/components/main/index.php
@@ -0,0 +1,38 @@
+<div class="w-full h-max grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-4 mt-10 mx-10 gap-4">
+    <div class="statcards">
+        <div class="flex gap-2">
+            <i class="fa-thin fa-2x fa-briefcase"></i>
+            <p>Total Investment</p>
+        </div>
+        <div class="statnum">
+            <span>0000</span>
+        </div>
+    </div>
+    <div class="statcards">
+        <div class="flex gap-2">
+            <i class="fa-thin fa-2x fa-briefcase"></i>
+            <p>Total Investment</p>
+        </div>
+        <div class="statnum">
+            <span>0000</span>
+        </div>
+    </div>
+    <div class="statcards">
+        <div class="flex gap-2">
+            <i class="fa-thin fa-2x fa-briefcase"></i>
+            <p>Total Investment</p>
+        </div>
+        <div class="statnum">
+            <span>0000</span>
+        </div>
+    </div>
+    <div class="statcards">
+        <div class="flex gap-2">
+            <i class="fa-thin fa-2x fa-briefcase"></i>
+            <p>Total Investment</p>
+        </div>
+        <div class="statnum">
+            <span>0000</span>
+        </div>
+    </div>
+</div>
