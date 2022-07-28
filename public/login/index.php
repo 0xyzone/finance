62e2e70f8bc688720052c6dc76775a7e24a61986
0xyzone <sumnsth@gmail.com>
2022-07-28 16:27:32 +0545

    LATEST UPDATE

--- public/login/index.php
+++ public/login/index.php
@@ -10,13 +10,33 @@ if (isset($_SESSION['vcaf_user'])) {
         <div class="text-2xl font-bold text-white">Log In</div>
         <form action="login.php" method="POST" id="signup">
             <input type="text" name="uname" id="uname" placeholder="Username/Email Address/Phone" autofocus="on" required>
-            <input type="password" name="pw" id="pw" placeholder="Password" required>
+            <div class="relative w-full">
+                <input type="password" name="pw" id="pw" placeholder="Password" required class="w-full z-0">
+                <a href="javascript:void(0)" class="showhide" id="showhide"><i class="fa-solid absolute right-1 fa-eye" id="eye"></i></a>
+            </div>
             <div class="flex mx-auto gap-2 items-center mt-2">
-        <button for="signup" type="submit" class="w-max btn-primary mx-auto">Log In</button>
-        <p class="text-gray-500">or</p>
-        <a href="<?php echo link; ?>signup" class="w-max btn-primary mx-auto ">Sign Up</a>
-    </div>
+                <button for="signup" type="submit" class="w-max btn-primary mx-auto">Log In</button>
+                <p class="text-gray-500">or</p>
+                <a href="<?php echo link; ?>signup" class="w-max btn-primary mx-auto ">Sign Up</a>
+            </div>
+            <div>
+                <p>If you forgot your password then GG! Arko banauna pardaina bcz mai hu na hehe lol :D </p>
+            </div>
         </form>
+        <script>
+            $('#showhide').click(function() {
+                // alert("Handler for .click() called.");
+                if ($('#pw').attr('type') == "password") {
+                    $('#pw').attr('type', 'text');
+                    $('#eye').removeClass('fa-eye');
+                    $('#eye').addClass('fa-eye-slash');
+                } else {
+                    $('#pw').attr('type', 'password');
+                    $('#eye').removeClass('fa-eye-slash');
+                    $('#eye').addClass('fa-eye');
+                }
+            });
+        </script>
     </section>
 
 <?php
--- public/signup/index.php
+++ public/signup/index.php
@@ -4,14 +4,31 @@ include "../includes/header.php";
 <section class="signup">
     <div class="text-2xl font-bold text-white">Sign Up</div>
     <form action="signup.php" method="POST" id="signup">
-    <input type="text" name="uname" id="uname" placeholder="Username" autofocus="on" required>
-    <input type="email" name="email" id="email" placeholder="Email Address" required>
-    <input type="tel" name="tel" id="tel" placeholder="Contact No." required>
-    <input type="password" name="pw" id="pw" placeholder="Password" required>
-    <div class="flex mx-auto gap-2 items-center mt-2">
-        <button for="signup" type="submit" class="w-max btn-primary mx-auto">Sign Up</button>
-        <p class="text-gray-500">or</p>
-        <a href="<?php echo link; ?>login" class="w-max btn-primary mx-auto ">Log In</a>
-    </div>
-</form>
+        <input type="text" name="uname" id="uname" placeholder="Username" autofocus="on" required>
+        <input type="email" name="email" id="email" placeholder="Email Address" required>
+        <input type="tel" name="tel" id="tel" placeholder="Contact No." required>
+        <div class="relative w-full">
+            <input type="password" name="pw" id="pw" placeholder="Password" required class="w-full z-0">
+            <a href="javascript:void(0)" class="showhide" id="showhide"><i class="fa-solid absolute right-1 fa-eye" id="eye"></i></a>
+        </div>
+        <div class="flex mx-auto gap-2 items-center mt-2">
+            <button for="signup" type="submit" class="w-max btn-primary mx-auto">Sign Up</button>
+            <p class="text-gray-500">or</p>
+            <a href="<?php echo link; ?>login" class="w-max btn-primary mx-auto ">Log In</a>
+        </div>
+    </form>
+    <script>
+        $('#showhide').click(function(){
+            // alert("Handler for .click() called.");
+            if($('#pw').attr('type') == "password"){
+                $('#pw').attr('type', 'text');
+                $('#eye').removeClass('fa-eye');
+                $('#eye').addClass('fa-eye-slash');
+            } else {
+                $('#pw').attr('type','password');
+                $('#eye').removeClass('fa-eye-slash');
+                $('#eye').addClass('fa-eye');
+            }
+        });
+    </script>
 </section>
