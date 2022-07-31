8f0f248737e0618299785ef0d89b5676a744e9aa
Oxy <sumnsth@gmail.com>
2022-07-31 17:37:20 +0545

    added menuvars and updated nav

--- public/components/navbar/index.php
+++ public/components/navbar/index.php
@@ -4,11 +4,13 @@ include 'menuvars.php';
 
 <div class="w-16 h-auto bg-slate-500 flex flex-col justify-between py-3 gap-2 fixed top-0 bottom-0 z-10">
     <div class="flex flex-col gap-2">
-        <a href="#" class="navbtn"><i class="fa-solid fa-chart-line"></i></a>
-        <a href="#" class="navbtn"><i class="fa-solid fa-chart-line"></i></a>
-        <a href="#" class="navbtn"><i class="fa-solid fa-chart-line"></i></a>
+        <?php foreach($menuitems as $menu): ?>
+            
+        <a href="<?php echo $menu['link']; ?>" class="navbtn"><?php echo $menu['icon']; ?></a>
+        <?php endforeach; ?>
+        
     </div>
     <div class="flex flex-col gap-2">
-        <a href="<?php echo link ; ?>logout.php" class="navbtn" onclick="confirm('Are you sure you want to logout?')"><i class="fa-solid fa-power-off"></i></a>
+        <a href="<?php echo link ; ?>logout.php" class="navbtn" onclick="return confirm('Are you sure you want to logout?')"><i class="fa-solid fa-power-off"></i></a>
     </div>
 </div>
---
+++ public/components/navbar/menuvars.php
@@ -0,0 +1,14 @@
+<?php
+$menuitems = array(
+    "home" =>
+    [
+        "name" => "Dashboard",
+        "link" => "?page=dashboard",
+        "icon" => "<i class='fa-solid fa-chart-line'></i>"
+    ],
+    "transactions" =>
+    [
+        "link" => "?page=transactions",
+        "icon" => "<i class='fa-solid fa-circle-plus'></i>"
+    ],
+);
